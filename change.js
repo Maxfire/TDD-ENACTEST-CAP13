a85d43e9d461e8eb644e28f52a3dbc11725b9640
Pacoocabreraa00 <158740232+Pacoocabreraa00@users.noreply.github.com>
2024-07-01 15:32:49 +0200

    Solucion Francisco de Asis Cabrera Hens

--- change.js
+++ change.js
@@ -8,25 +8,22 @@ var coins = [200, 100, 50, 20, 10, 5, 2, 1];
  * @example
  * getChange(215, 300); // returns [50, 20, 10, 5]
  */
-function getChange (payable, paid) {
+
+//HACER VARIANTE DE getChange
+function getChange(payable, paid) {
   var change = [];
-  var length = coins.length;
-  var remaining = paid - payable; // we reduce this below
+  var remaining = paid - payable;
 
-  for (var i = 0; i < length; i++) { // loop through array of notes & coins:
+  for (var i = 0; i < coins.length; i++) {
     var coin = coins[i];
-
-    var times_coin_fits = Math.floor(remaining / coin); // no partial coins
-    if(times_coin_fits >= 1) { // check coin fits into the remaining amount
-      
-      for(var j = 0; j < times_coin_fits ; j++) { // add coin to change x times
-        change.push(coin);
-        remaining = remaining - coin;  // subtract coin from remaining
-      }
+    while (remaining >= coin) {
+      change[change.length] = coin;
+      remaining -= coin;
     }
   }
+
   return change;
-};
+}
 
 /* The code block below ONLY Applies to Node.js - This Demonstrates
    re-useability of JS code in both Back-end and Front-end! #isomorphic */
