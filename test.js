3f53f71f6ad2d1c99f3a6f975bd13d43e2f7a583
gonzavalde23 <gonzavalde23@gmail.com>
2024-07-03 19:52:08 +0200

    Solucion del ejercicio

--- test.js
+++ test.js
@@ -13,21 +13,39 @@ test('getChange(1,1) should equal [] - an empty array', function(assert) {
   assert.deepEqual(result, expected);
 });
 
-test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
-  var result = getChange(215, 300); // expect an array containing [50,20,10,5]
-  var expected = [50, 20, 10, 5];
+// test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
+//   var result = getChange(215, 300); // expect an array containing [50,20,10,5]
+//   var expected = [50, 20, 10, 5];
+//   assert.deepEqual(result, expected);
+// });
+
+// test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
+//   var result = getChange(486, 600);
+//   var expected = [100, 10, 2, 2];
+//   assert.deepEqual(result, expected);
+// });
+
+// test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
+//   var result = getChange(12, 400);
+//   var expected = [200, 100, 50, 20, 10, 5, 2, 1];
+//   assert.deepEqual(result, expected);
+// });
+
+test('getChange(275, 500) should return [200, 20, 5]', function(assert) {
+  var result = getChange(275, 500); // expect an array containing [200, 20, 5]
+  var expected = [200, 20, 5];
   assert.deepEqual(result, expected);
 });
 
-test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
-  var result = getChange(486, 600);
-  var expected = [100, 10, 2, 2];
+test('getChange(99, 200) should return [100, 1]', function(assert) {
+  var result = getChange(99, 200);
+  var expected = [100, 1];
   assert.deepEqual(result, expected);
 });
 
-test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
-  var result = getChange(12, 400);
-  var expected = [200, 100, 50, 20, 10, 5, 2, 1];
+test('getChange(50, 100) should return [50]', function(assert) {
+  var result = getChange(50, 100);
+  var expected = [50];
   assert.deepEqual(result, expected);
 });
 
